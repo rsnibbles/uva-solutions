f0f2ca538789fe51359c7e74d1e9429d2c170b91
Robert 'Nybbles' Studenic <rs.nibbles@gmail.com>
2013-02-10 20:00:13 -0500

    'No: Wrong Answer' solution. Bored now.

--- 673/673.cc
+++ 673/673.cc
@@ -1,6 +1,39 @@
 #include <iostream>
-#include <string>
-
+#include <cstring>
+using namespace std;
 int main() {
+	unsigned int numCases = 0;
+	cin >> numCases;
 
+	for (int index = 0; index < numCases; ++index) {
+		char sequence[128];
+		cin >> sequence;
+
+		char lastMatched = ' ';
+		long unmatchedParenthesis = 0;
+		long unmatchedBracket = 0;
+
+		for (int i = 0; i < strlen(sequence); ++i) {
+			if (sequence[i] == '(') {
+				lastMatched = '(';
+				++unmatchedParenthesis;
+			}
+			else if (sequence[i] == ')' && lastMatched == '(') {
+				--unmatchedParenthesis;
+			}
+			else if (sequence[i] == '[') {
+				lastMatched = '[';
+				++unmatchedBracket;
+			}
+			else if (sequence[i] == ']' && lastMatched == '[') {
+				--unmatchedBracket;
+			}
+		//cerr << "PARA " << unmatchedParenthesis << endl;
+		//cerr << "BRAK " << unmatchedBracket << endl;
+		}
+		if (unmatchedParenthesis == 0 && unmatchedBracket == 0)
+			cout << "Yes" << endl;
+		else
+			cout << "No" << endl;
+	}
 }
