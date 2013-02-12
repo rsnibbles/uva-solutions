ac26b09b7f641bf612493a7e78c6bc392bab15b1
Robert 'Nybbles' Studenic <ras85@zips.uakron.edu>
2013-02-12 17:36:52 -0500

    Added code for problem 272.

---
+++ 272/tex.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string line;
+	while (getline(cin, line)) {
+		cout << line << endl;
+	}
+}
